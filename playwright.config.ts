92a5068e568d782f2737e16f1367ecad8e601d16
Julio Guerra <j.guerratamayo@gmail.com>
2023-05-02 11:02:53 +0200

    Set headless true and screenshot on in config

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,8 +28,9 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    trace: "retain-on-failure",
     headless: true,
+    screenshot: "on",
   },
 
   /* Configure projects for major browsers */
