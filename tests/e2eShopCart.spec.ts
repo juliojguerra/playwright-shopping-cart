79708e978ad7670703b3ec20e4db48307d4a51e3
Julio Guerra <j.guerratamayo@gmail.com>
2023-05-02 11:01:29 +0200

    Add main e2eShopCart test

---
+++ tests/e2eShopCart.spec.ts
@@ -0,0 +1,35 @@
+import { test } from "@playwright/test";
+import { POManager } from "../pageobjects/POManager";
+import data from "../utils/productsTestData.json";
+
+test("E2E Shop two items", async ({ page }) => {
+  const poManager = new POManager(page);
+
+  // Dashboard Page
+  const dashboardPage = poManager.getDashboardPage();
+
+  await dashboardPage.goto();
+  await dashboardPage.addProductToCart(data.firstProductName);
+  await dashboardPage.addProductToCart(data.secondProductName);
+
+  // Product Page
+  const productPage = poManager.getProductPage();
+
+  await productPage.verifyProductPageIsDisplayed(data.secondProductName);
+  await productPage.selectColor(data.colorOptions["Blue"]);
+  await productPage.fillQuantity(data.secondProductQty);
+  await productPage.addToCart();
+  await productPage.verifyProductWasAddedToCart(data.secondProductName);
+  await productPage.viewCart();
+
+  // Shopping Cart Page
+  const cartPage = poManager.getCartPage();
+
+  await cartPage.verifyProductsWereAdded(
+    data.firstProductName,
+    data.secondProductName,
+    Object.keys(data.colorOptions)[1]
+  );
+
+  await page.pause();
+});
